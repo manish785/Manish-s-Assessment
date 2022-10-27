2073c1c33263c1841cbe4baf651de0a541e856f4
manish785 <kumarachilish1997@gmail.com>
2022-10-27 13:10:01 +0530

    Code Assessment

---
+++ app.js
@@ -0,0 +1,51 @@
+const data= [
+    { "Gender": "Male", "HeightCm": 171, "WeightKg":96},
+    {"Gender": "Male", "HeightCm": 161, "WeightKg":85}, 
+    {"Gender": "Male", "HeightCm": 180, "WeightKg":77}, 
+    {"Gender": "Female", "HeightCm": 166,"WeightKg":62}, 
+    {"Gender": "Female", "HeightCm": 150, "WeightKg":70}, 
+    {"Gender": "Female","HeightCm": 167, "WeightKg":82}
+];
+
+
+let overWeight = 0;
+function BMI() {
+
+    this.bmi = Math.round(this.WeightKg / (this.HeightCm / 100 * this.HeightCm / 100));
+    
+    
+    if (this.bmi <= 18.4) {
+        this.bmiCategory = "Underweight";
+        this.healthRisk = "Malnutrition Risk";
+    } else if (this.bmi <= 24.9) {
+        this.bmiCategory = "Normal Weight";
+        this.healthRisk = "Low Risk";
+    } else if (this.bmi <= 29.9) {
+        this.bmiCategory = "Overweight";
+        this.healthRisk = "Enhanced Risk";
+        overWeight++;
+    } else if (this.bmi <= 34.9) {
+        this.bmiCategory = "Moderatly Obese";
+        this.healthRisk = "Medium Risk";
+        overWeight++;
+    } else if (this.bmi <= 39.9) {
+        this.bmiCategory = "Severly Obese";
+        this.healthRisk = "High Risk";
+        overWeight++;
+    } else {
+        this.bmiCategory = "Very Severly Obese";
+        this.healthRisk = "Very High Risk";
+        overWeight++;
+    }
+
+}
+
+for(let i=0;i<data.length;i++){
+    BMI.apply(data[i]);
+    console.log(data[i]);
+}
+
+
+console.log("Number of People who are Overweight is: " + overWeight);
+
+
